38c73d1d6a3a155c63922d99a8f179df9aad5eba
Yogita Shete <yogitashete24@gmail.com>
2024-01-08 16:30:48 +0530

    server hosting code

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 dotenv.config();
 
+import path from 'path'
+
+const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json());
 
@@ -12,6 +16,11 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json({extended : true}));
 app.use('/', routes);
+
+
+if (process.env.NODE_ENV === 'production') { app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+
+app.get('*', (req, res) => { res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html')) }); }
 
 const PORT = process.env.PORT || 5001;
 
